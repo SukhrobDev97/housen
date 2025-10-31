dcb9cb1b6b09acd18e7b47672754680a6dcf7547
Sukhrob <zeboismailova@Sukhrob-MacBookPro.local>
2025-10-31 15:07:55 +0900

    create components related to modules

--- apps/housen-api/src/components/components.module.ts
+++ apps/housen-api/src/components/components.module.ts
@@ -1,8 +1,25 @@
 import { Module } from '@nestjs/common';
 import { MemberModule } from './member/member.module';
 import { ProjectModule } from './project/project.module';
+import { AuthModule } from './auth/auth.module';
+import { CommentModule } from './comment/comment.module';
+import { LikeModule } from './like/like.module';
+import { ViewModule } from './view/view.module';
+import { FollowModule } from './follow/follow.module';
+import { CommunityModule } from './community/community.module';
+import { BoardArticleModule } from './board-article/board-article.module';
 
 @Module({
-  imports: [MemberModule, ProjectModule]
+  imports: [
+    MemberModule,
+    ProjectModule, 
+    AuthModule, 
+    CommentModule, 
+    LikeModule, 
+    ViewModule, 
+    FollowModule, 
+    CommunityModule, 
+    BoardArticleModule
+  ]
 })
 export class ComponentsModule {}
