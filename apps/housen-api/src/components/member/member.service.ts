8fa3f0898591231f26688da7bcff1c3aca69bf7e
Sukhrob <zeboismailova@Sukhrob-MacBookPro.local>
2025-11-02 23:09:36 +0900

    feat: hashing password with bycriptjs

--- apps/housen-api/src/components/member/member.service.ts
+++ apps/housen-api/src/components/member/member.service.ts
@@ -5,13 +5,20 @@ import { LoginInput, MemberInput } from '../../libs/dto/member/member.input';
 import { Member } from '../../libs/dto/member/member';
 import { Message } from '../../libs/enums/common.enum';
 import { MemberStatus } from '../../libs/enums/member.enum';
+import { AuthService } from '../auth/auth.service';
 
 @Injectable()
 export class MemberService {
-    constructor(@InjectModel("Member") private readonly memberModel: Model<Member>) {}
+    constructor(
+      @InjectModel("Member") private readonly memberModel: Model<Member>,
+      private authService: AuthService,
+    ) {}
 
 
     public async signup(input: MemberInput): Promise<Member> {
+      //Hash password;
+      input.memberPassword = await this.authService.hashPassword(input.memberPassword)
+
       try{
           const result = await this.memberModel.create(input)
           return result;
@@ -35,7 +42,8 @@ export class MemberService {
       }
     
       // TODO: Compare passwords
-      const isMatch = memberPassword === response.memberPassword;
+      const isMatch = await this.authService.comparePasswords(input.memberPassword!, response.memberPassword!)
+
       if (!isMatch) throw new InternalServerErrorException(Message.WRONG_PASSWORD);
     
       return response;
