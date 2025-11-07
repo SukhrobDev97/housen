fdbe90d95b7b9f348c53bc34b19bf12929126386
Sukhrob <zeboismailova@Sukhrob-MacBookPro.local>
2025-11-07 11:52:39 +0900

    feat: configure comment module & comment related resolver & service model

--- apps/housen-api/src/components/comment/comment.module.ts
+++ apps/housen-api/src/components/comment/comment.module.ts
@@ -1,4 +1,23 @@
 import { Module } from '@nestjs/common';
+import { CommentService } from './comment.service';
+import { CommentResolver } from './comment.resolver';
+import CommentSchema from '../../schemas/Comment.model';
+import { AuthModule } from '../auth/auth.module';
+import { MemberModule } from '../member/member.module';
+import { MongooseModule } from '@nestjs/mongoose';
+import { BoardArticleModule } from '../board-article/board-article.module';
+import { ProjectModule } from '../project/project.module';
 
-@Module({})
+@Module({
+  imports:[
+    MongooseModule.forFeature([
+      {name: "Comment", schema: CommentSchema }
+    ]), 
+    AuthModule,
+    MemberModule,
+    ProjectModule,
+    BoardArticleModule
+  ],
+  providers: [CommentResolver, CommentService]
+})
 export class CommentModule {}
