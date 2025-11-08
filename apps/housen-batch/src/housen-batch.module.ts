54d86b6bd4f53cb5826b427f7ce1ed35e8120414
Sukhrob <zeboismailova@Sukhrob-MacBookPro.local>
2025-11-08 15:33:22 +0900

    fix: modify batch modile & connect mongo db

--- apps/housen-batch/src/housen-batch.module.ts
+++ apps/housen-batch/src/housen-batch.module.ts
@@ -2,9 +2,11 @@ import { Module } from '@nestjs/common';
 import { HousenBatchController } from './housen-batch.controller';
 import { HousenBatchService } from './housen-batch.service';
 import { ConfigModule } from '@nestjs/config';
+import { DatabaseModule } from './database/database.module';
+import { ScheduleModule } from '@nestjs/schedule';
 
 @Module({
-  imports: [ConfigModule.forRoot()],
+  imports: [ConfigModule.forRoot(), DatabaseModule, ScheduleModule.forRoot()],
   controllers: [HousenBatchController],
   providers: [HousenBatchService],
 })
